698bfa522bb4882bcf7ebb6fc8894ec5c5e672e3
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-10 12:00:29 +0300

    Adding JAvascript for interactivity of the website

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Add this JavaScript code in a new file named main.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('nav');
+
+    // Toggle mobile menu
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+    });
+
+    // Close mobile menu when a menu item is clicked
+    document.querySelectorAll('nav a').forEach(item => {
+        item.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+        });
+    });
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
